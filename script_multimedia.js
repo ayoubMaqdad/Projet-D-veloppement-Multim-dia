99aa3e6b197fc3e35cba071c4dd715f4f441ed72
ayoubMaqdad <ayoub.maqdad@gmail.com>
2019-04-19 00:57:35 +0200

    css

--- script_multimedia.js
+++ script_multimedia.js
@@ -1,70 +1,67 @@
-// Set constraints for the video stream
+var MyCam;
+var canvas ;
+var btnCapture;
+var imcanvas;
+var captureFlag = false;
+
+function watch_video(){
+
+    document.getElementById("divs").style.display = "block";
+
+    MyCam = document.getElementById("MyCam");
+    canvas = document.getElementById("canvas");
+    btnCapture = document.getElementById("btnCapture");
 
 
-var interval;
-var minutes = 0;
-var seconds = 10;
+    imcanvas = canvas.getContext("2d");
+
+    btnCapture.addEventListener("click", capture);
 
 
-function countdown(element) {
-    interval = setInterval(function() {
-        var el = document.getElementById(element);
-        if(seconds == 0) {
-            if(minutes == 0) {
-                alert(el.innerHTML = "countdown's over!");
-                clearInterval(interval);
-                return;
-            } else {
-                minutes--;
-                seconds = 60;
-            }
-        }
-        if(minutes > 0) {
-            var minute_text = minutes + (minutes > 1 ? ' minutes' : ' minute');
-        } else {
-            var minute_text = '';
-        }
-        var second_text = seconds > 1 ? 'seconds' : 'second';
-        el.innerHTML = minute_text + ' ' + seconds + ' ' + second_text + ' remaining';
-        seconds--;
-    }, 1000);
-}
-var constraints = { video: { facingMode: "user" }, audio: false };
-// Define constants
-const cameraView = document.querySelector("#camera--view"),
-    cameraOutput = document.querySelector("#camera--output"),
-    cameraSensor = document.querySelector("#camera--sensor"),
-    cameraTrigger = document.querySelector("#camera--trigger")
+    navigator.getUserMedia = (
 
-function cameraStart() {
-    navigator.mediaDevices
-        .getUserMedia(constraints)
-        .then(function(stream) {
-            track = stream.getTracks()[0];
-            cameraView.srcObject = stream;
-        })
-        .catch(function(error) {
-            console.error("Oops. Something is broken.", error);
-        });
+        navigator.getUserMedia ||
+        navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia ||
+        navigator.msGetUserMedia );
+
+    if(navigator.getUserMedia){
+
+        navigator.getUserMedia({
+            video : true,
+        }, SuccessCapture, ErrorCapture);
+
+    }else{
+        var source = document.createElement('source');
+        source.setAttribute('src', 'video.mp4');
+        MyCam.appendChild(source);
+        MyCam.play();
+    }
+
 }
 
-cameraTrigger.onclick = function() {
+function SuccessCapture(stream){
+    MyCam.srcObject = stream;
+}
+
+function ErrorCapture(error){
+    console.log("error " + error);
+    var source = document.createElement('source');
+    source.setAttribute('src', 'video.mp4');
+    MyCam.appendChild(source);
+    MyCam.play();
+}
+
+function capture(){
     var seconds = document.getElementById("countdown").textContent;
     var countdown = setInterval(function() {
         seconds--;
         document.getElementById("countdown").textContent = seconds;
         if (seconds <= 0){
             clearInterval(countdown);
-            cameraSensor.width = cameraView.videoWidth;
-            cameraSensor.height = cameraView.videoHeight;
-            cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
-            cameraOutput.src = cameraSensor.toDataURL("image/webp");
-            cameraOutput.classList.add("taken");
-
+            captureFlag = true;
+            imcanvas.drawImage(MyCam, 0, 0, canvas.width, canvas.height);
         }
     }, 1000);
 
-};
-
-// Start the video stream when the window loads
-window.addEventListener("load", cameraStart, false);
+}
